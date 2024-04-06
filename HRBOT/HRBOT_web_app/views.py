18c5ff7b67b22fa12dee3511bd9492aabfbee6b5
Jayesh Patil <jayeshpatil8l30@gmail.com>
2024-04-06 13:33:59 +0530

    added static/templates

--- HRBOT/HRBOT_web_app/views.py
+++ HRBOT/HRBOT_web_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'sign_up.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+   
+def chatbot(request):
+    return render(request, 'index.html')
